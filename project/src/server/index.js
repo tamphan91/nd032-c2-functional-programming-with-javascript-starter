c1b8c489483095b499f51f22cd48ca7665ddb7cb
9Tam_Phan <9tam_phan@singaporeair.com.sg>
2024-08-27 16:09:12 +0700

    phase 1

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -1,28 +1,39 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
-const path = require('path')
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+const path = require("path");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, '../public')))
+app.use("/", express.static(path.join(__dirname, "../public")));
 
-// your API calls
+// get rovers list
+app.get("/rovers", async (req, res) => {
+  try {
+    let images = await fetch(
+      `${process.env.MARS_ROVERS_API_URL}?api_key=${process.env.API_KEY}`
+    ).then((res) => res.json());
+    res.send({ images });
+  } catch (err) {
+    console.log("error:", err);
+  }
+});
 
-// example API call
-app.get('/apod', async (req, res) => {
-    try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
-    } catch (err) {
-        console.log('error:', err);
-    }
-})
+// get rover by name
+app.get("/rovers/:name", async (req, res) => {
+  try {
+    let images = await fetch(
+      `${process.env.MARS_ROVERS_API_URL}/${req.params.name}/photos?sol=1000&api_key=${process.env.API_KEY}`
+    ).then((res) => res.json());
+    res.send({ images });
+  } catch (err) {
+    console.log("error:", err);
+  }
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
