e71789b3c50276373a47fe35100051f8fc488c42
tamphan91 <tamphan04101991@gmail.com>
2024-08-29 21:57:33 +0700

    finish coding

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,5 +1,7 @@
 let store = Immutable.Map({
   selectedRover: null,
+  photos: [],
+  isFetching: true,
   rovers: [],
 });
 
@@ -17,43 +19,52 @@ const render = async (rootElement, state) => {
 
 // create content
 const App = (state) => {
-  let rovers = state.get("rovers");
-  let selectedRover = state.get("selectedRover");
-  console.log("rovers", rovers);
-  console.log("selectedRover", selectedRover);
+  const rovers = state.get("rovers");
+  const selectedRover = state.get("selectedRover");
+  const photos = state.get("photos");
+  const isFetching = state.get("isFetching");
   return `
         <div class="bg-white py-24 sm:py-32">
-            <div class="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-2">
-                <div class="max-w-2xl">
-                    <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Mars Dashboard</h2>
+            <div class="mx-auto grid max-w-7xl gap-x-8 gap-y-10 px-6 lg:px-8">
+                <div class="col-span-2 grid gap-y-10">
+                    <p class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">Mars Dashboard</p>
+                    <p class="text-1xl font-bold tracking-tight text-gray-500 sm:text-2xl">${
+                      selectedRover ? "Images" : "Rover List"
+                    }</p>
                 </div>
-                ${rovers.length > 0 ? renderRoversHtml(rovers) : 'Loading...'}
+                ${isFetching ? "Loading..." : ""}
+                ${
+                  rovers.length > 0 && !selectedRover
+                    ? renderListHtml(rovers, renderRoverHtml)
+                    : ""
+                }
+                ${selectedRover && !isFetching > 0 ? renderPhotosHtml(photos) : ""}
             </div>
         </div>
     `;
 };
 
 // listening for load event because page should load before any JS is called
-window.addEventListener("load", async () => {
+window.addEventListener("load", () => {
   render(root, store);
-  await getRovers((data) => {
-    updateStore(store.set("rovers", data.images.rovers));
+  getRovers((data) => {
+    updateStore(
+      store.set("isFetching", false).set("rovers", data.images.rovers)
+    );
   });
 });
 
-const renderRoversHtml = (rovers) => {
+const renderListHtml = (list, callback) => {
+  return `<ul role="list" class="grid gap-x-8 gap-y-8 md:grid-cols-2 md:gap-y-10 md:col-span-2 col-span-2">
+            ${list.map((element) => callback(element)).join("")}
+        </ul>`;
+};
 
-    return `<ul role="list" class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
-            ${rovers.map(rover => renderReverHtml(rover)).join("")}
-        </ul>`
-}
-
-const renderReverHtml = (rover) => {
-    const {name, launch_date, landing_date, status} = rover;
-    return `<li>
+const renderRoverHtml = (rover) => {
+  const { name, launch_date, landing_date, status } = rover;
+  return `<li>
             <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-                <button>
-                <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+                <div class="pt-6 md:p-8 text-left space-y-4">
                     <figcaption class="font-medium">
                         <div class="text-sky-500 dark:text-sky-400">
                             Name: ${name}
@@ -67,25 +78,65 @@ const renderReverHtml = (rover) => {
                          <div class="text-slate-700 dark:text-slate-500">
                             Status: ${status}
                         </div>
+                        <button onclick="handleShowImages('${name}')" class="bg-cyan-500 shadow-lg shadow-cyan-500/50 rounded-lg p-2 mt-4">Images</button>
                     </figcaption>
                 </div>
             </figure>
-        </li>`
-}
+        </li>`;
+};
 
-// const renderReverHtml = (rover) => {
-//     const {name, launch_date, landing_date, status} = rover;
-//     return `<li>
-//             <div class="flex items-center gap-x-6">
-//                 <div>
-//                     <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">Name: ${name}</h3>
-//                     <p class="text-sm font-semibold leading-6 text-indigo-600">Launch date: ${launch_date}</p>
-//                     <p class="text-sm font-semibold leading-6 text-indigo-600">Landing date: ${landing_date}</p>
-//                     <p class="text-sm font-semibold leading-6 text-indigo-600">Status: ${status}</p>
-//                 </div>
-//             </div>
-//         </li>`
-// }
+const renderPhotosHtml = (photos) => {
+
+  return `
+        <div class"flex flex-col">
+          <button onclick="handleBackToRover()" class="bg-cyan-500 shadow-lg shadow-cyan-500/50 rounded-lg p-2 m-4">Back to rover list</button>
+          ${renderListHtml(photos, renderPhotoHtml)}
+          ${photos.length === 0 ? 'No photo found!' : ''}
+        </div>
+    `;
+};
+
+const renderPhotoHtml = (photo) => {
+  const {
+    img_src,
+    camera: { full_name, name: cameraName },
+    earth_date,
+    rover: { name },
+  } = photo;
+  return `<li>
+            <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+              <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="${img_src}" alt="" width="384" height="512">
+              <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+                <blockquote>
+                  <p class="text-lg font-medium">
+                    “This photo was taken by ${full_name} on the ${name} rover on ${earth_date}.”
+                  </p>
+                </blockquote>
+                <figcaption class="font-medium">
+                  <div class="text-sky-500 dark:text-sky-400">
+                    Rover: ${name}
+                  </div>
+                  <div class="text-slate-700 dark:text-slate-500">
+                    ${full_name} (${cameraName})
+                  </div>
+                </figcaption>
+              </div>
+            </figure>
+        </li>`;
+};
+
+const handleShowImages = (roverName) => {
+  updateStore(store.set("isFetching", true).set("selectedRover", roverName));
+  getRoverByName(roverName, (data) => {
+    updateStore(
+      store.set("isFetching", false).set("photos", data.images.photos)
+    );
+  });
+};
+
+const handleBackToRover = () => {
+  updateStore(store.set("photos", []).set("selectedRover", null));
+};
 
 const getRovers = (callback) => {
   fetch("http://localhost:3000/rovers")
